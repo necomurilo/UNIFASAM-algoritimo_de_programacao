9b80bf301171fd70f10e4f378480fe40ae4155ec
Murilo Lima Neco <109995111+necomurilo@users.noreply.github.com>
2023-09-29 22:48:16 -0300

    atv org.

--- Exercises/1nd_semestre/atividade10.py
+++ Exercises/1nd_semestre/atividade10.py
@@ -1,8 +1,8 @@
 soma = 0.0
-  while True :
-  produto = float(input('Valor do produto ou 0:'))
-  if produto == 0 :
-break
+while True :
+    produto = float(input('Valor do produto ou 0:'))
+    if produto == 0 :
+        break
 soma = soma + produto
 print('Sub-total é ', soma)
 print('O total da compra é ' , soma)
