e7cdc6f68638f35bb59cc88d0653102f9c170149
Murilo Lima Neco <109995111+necomurilo@users.noreply.github.com>
2023-10-03 19:21:55 -0300

    informações

--- Exercises/2nd_semester_2023/Aula4/sorteioNumerosAleatorios.py
+++ Exercises/2nd_semester_2023/Aula4/sorteioNumerosAleatorios.py
@@ -1,7 +1,7 @@
-import random
-numeros = []
-while len(numeros) < 10:
-    numero = random.randint(1,1000)
-    if numero not in numeros:
+import random # gera números aleatórios
+numeros = []  # inicia uma lista vazia
+while len(numeros) < 10: #permitir que seja sorteado 10 numeros
+    numero = random.randint(1,1000) # gera um numero dentro do intervalo
+    if numero not in numeros: # usa se o numero nao estiver na lista
         numeros.append(numero)
         print(sorted(numeros))
