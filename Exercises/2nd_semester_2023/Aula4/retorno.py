299e01b84024af749d727e045c7288bb1361ba1f
Murilo Lima Neco <109995111+necomurilo@users.noreply.github.com>
2023-10-04 15:46:25 -0300

    Update retorno.py

--- Exercises/2nd_semester_2023/Aula4/retorno.py
+++ Exercises/2nd_semester_2023/Aula4/retorno.py
@@ -1,4 +1,12 @@
-nomes = [(0, 'Pedro'), (1, 'Maria'), (2, 'Manoela')]
-dicionario = dict(nomes)
-del dicionario[0]
-print(dicionario)
+# nomes = [(0, 'Pedro'), (1, 'Maria'), (2, 'Manoela')]
+# dicionario = dict(nomes)
+# del dicionario[0]
+# print(dicionario)
+
+
+I   – def __init__(self):
+
+II  – def __init__(this):
+III – def __init__(super):
+IV  – def __init__():
+V   – __init__(self):
